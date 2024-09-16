c855f2f1862f6ac0d6c4cbb033c1f304ebf67760
Nancy Debárbora <nancydebarbora@yahoo.com.ar>
2024-09-16 18:44:34 -0300

    idem

--- ENTORNO/INFO/INFO/settings.py
+++ ENTORNO/INFO/INFO/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
@@ -116,7 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_DIRS = [
+BASE_DIR / 'static',
+]
 STATIC_URL = 'static/'
+
+
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT= BASE_DIR/'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
