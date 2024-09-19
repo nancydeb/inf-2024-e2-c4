ef3da95d82340fb0b8ac033685a993e697157fdb
Nancy Debárbora <nancydebarbora@yahoo.com.ar>
2024-09-18 23:11:20 -0300

    create new project starwars characters

---
+++ ENTORNO/INFO/VOTOS/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+from .models import Persona
+
+from django.template import loader
+# GET
+# POST
+# DELETE
+# PUT
+def index(request):
+    return HttpResponse("Hola mundo desde la web!")
+
+def persona_id(request, id_persona):
+    response = "La persona con id: es %s"
+    return HttpResponse(response % id_persona)
+
+def personas_list(request):
+    personas_list = Persona.objects.all()
+    print(personas_list)
+    return HttpResponse(personas_list)
+
+def personas_template(request):
+    personas_list = Persona.objects.all()
+    template = loader.get_template("personas_template.html")
+    context = { "personas_list": personas_list, "title": "Informatorio" }
+    return HttpResponse(template.render(context, request)) # lista.add(1)
